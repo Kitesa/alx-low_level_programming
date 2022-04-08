f75fdb121b8571900ab0fa44c6b69c049ef1f12e
Kitesa <kitesamerga@gmail.com>
2022-04-08 02:01:51 -0700

    Added a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line.

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 int main(void) 
+//DEscription
 {
-//FIrst C programming in alx
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
