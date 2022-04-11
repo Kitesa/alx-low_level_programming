3a24db6ced945a12bc6418e26d163b2ca5e56c80
Kitesa <kitesamerga@gmail.com>
2022-04-11 00:47:42 -0700

    Added a program that will print whether the number stored in the variable n is positive or negative.

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,27 +14,12 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n>0) 
 	{
-		/* 
-		 *
-		 * Print n is positive if it is greater than zero
-		 *
-		 */
 		printf("%d is positive\n", n);
 	} else if(n==0)
 	{
-		/* 
-		 *
-		 * Print n is zero if it is equal to zero
-		 *
-		 */
 		printf("%d is zero\n", n);
 	} else 
 	{
-		/*
-		 *
-		 * Print n is negative if it is less than zero
-		 *
-		 */
 		printf("%d is negative\n", n);
 	}
 return (0);
