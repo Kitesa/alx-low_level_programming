00bc18e570ff5df1b4395cac4aab6d0905c119e9
Kitesa <kitesamerga@gmail.com>
2022-04-12 21:41:09 -0700

    Added a function that prints the alphabet, in lowercase, followed by a new line.

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ *
+ * main - prints 10 times the alphabet, in lowercase, followed by a new line.
+ *
+ * Return: void after completion
+ *
+ */
+int main(void)
+{
+void print_alphabet(void)
+{
+char counter = 'a';
+
+while (counter <= 'z')
+{
+putchar(counter);
+}
+return (void);
+}
+putchar('\n');
+return (0);
+}
