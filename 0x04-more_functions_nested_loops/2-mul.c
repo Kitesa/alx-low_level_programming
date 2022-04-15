cce0136aac3b38ca98182ccae2df9f4d3423b66a
Kitesa <kitesamerga@gmail.com>
2022-04-15 00:09:42 -0700

    Added a function that returns productof two number

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - product of two numbers
+ *
+ * @a: first number
+ * @b: second number
+ *
+ * Return: always zero
+ *
+ */
+int mul(int a, int b)
+{
+	int c = a*b;
+	return (c);
+}
