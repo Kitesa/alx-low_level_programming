b2b61dc65b44d34db366ff13a39dfdac5458596a
Kitesa <kitesamerga@gmail.com>
2022-04-22 01:56:19 -0700

    Added one line to this code, so that the program prints a[2] = 98, followed by a new line.

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,20 +1,15 @@
-
-<<<<<<< HEAD
-=======
 #include <stdio.h>
 
->>>>>>> 49c7f42154a885aa99894295fadefe81f60b8cf4
 int main(void)
 {
-  int n;
-  int a[5];
-  int *p;
+	int n;
+	int a[5];
+	int *p;
 
-<<<<<<< HEAD
 	a[2] = 1024;
 	p = &n;
 	/*
-	 * Write your line of code here...
+	 * write your line of code here...
 	 * Remember:
 	 * - you are not allowed to use a
 	 * - you are not allowed to modify p
@@ -22,23 +17,7 @@ int main(void)
 	 * - you are not allowed to code anything else than this line of code
 	 */
 	*(p + 5) = 98;
-	/* ... so that this prints 98\n */
+	/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
-=======
-  a[2] = 1024;
-  p = &n;
-  /*
-   * write your line of code here...
-   * Remember:
-   * - you are not allowed to use a
-   * - you are not allowed to modify p
-   * - only one statement
-   * - you are not allowed to code anything else than this line of code
-   */
-  *(p + 5) = 98;
-  /* ...so that this prints 98\n */
-  printf("a[2] = %d\n", a[2]);
-  return (0);
->>>>>>> 49c7f42154a885aa99894295fadefe81f60b8cf4
 }
