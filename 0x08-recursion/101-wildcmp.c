b67f02bc0cda93000030d303ed883b73e95772e2
Kitesa <kitesamerga@gmail.com>
2022-04-27 01:59:57 -0700

    Added a function that compares two strings and returns 1 if the strings can be considered identical, otherwise return 0.

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -2,8 +2,8 @@
 
 /**
  * wildcmp - function with two argument
- * @s1 - char type pointer 1st argument
- * @s2 - char type pointer 2nd argument
+ * @s1: char type pointer 1st argument
+ * @s2: char type pointer 2nd argument
  *
  * Description: compare two strings and return
  * Return: 1 for identical, 0 for other
