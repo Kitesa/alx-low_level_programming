f0a011f20f7abe2141c1a054e4e98424d37f6248
Kitesa <kitesamerga@gmail.com>
2022-05-20 00:53:30 -0700

    Added  a function that prints all the elements of a list_t list.

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,3 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
 #include <stdio.h>
 
 /**
@@ -23,4 +21,3 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
-#endif
