2c4e99cee018a728cf08c38e5574829033666ea4
Kitesa <kitesamerga@gmail.com>
2022-05-25 07:02:37 -0700

    Added a function that sets head to NULL

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,40 +1,37 @@
 #include "lists.h"
-
+#include <stdio.h>
 /**
- * free_listint_safe - frees a linked list
- * @h: pointer to the first node in the linked list
+ * print_listint_safe - function with one argument
+ * @head: A pointer to the head node of linked list
  *
- * Return: number of elements in the freed list
+ * Description: prints a listint_list safely
+ * REturn: number of nodes in the list
  */
-size_t free_listint_safe(listint_t **h)
+size_t print_listint_safe(const listint_t *head)
 {
-  size_t len = 0;
-  int diff;
-  listint_t *temp;
+	size_t node, index = 0;
 
-  if (!h || !*h)
-    return (0);
+	node = counter_listint_len(head);
 
-  while (*h)
-    {
-      diff = *h - (*h)->next;
-      if (diff > 0)
+	if (node == 0)
 	{
-	  temp = (*h)->next;
-	  free(*h);
-	  *h = temp;
-	  len++;
+		for (; head != NULL; node++)
+		{
+			printf("[%p] %d\n", (void *)head, head->n);
+			head = head->next;
+		}
 	}
-      else
+
+	else
 	{
-	  free(*h);
-	  *h = NULL;
-	  len++;
-	  break;
+		for(index = 0; index < node; index++)
+		{
+			printf("[%p] %d\n", (void *)head, head->n);
+			head = head->next;
+		}
+
+		printf("-> [%p] %d\n", (void *)head, head->n);
 	}
-    }
 
-  *h = NULL;
-
-  return (len);
+	return (node);
 }
