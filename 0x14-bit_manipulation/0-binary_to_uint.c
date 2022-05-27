5778a452869e5c29d803ef1ba1630d210cd28798
Kitesa <kitesamerga@gmail.com>
2022-05-27 01:58:50 -0700

    Added a function that converts a binary number to unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,55 +1,24 @@
 #include "main.h"
-#include <stdio.h>
-
-int _power_recursion(int x, int y);
-
 /**
- * binary_to_uint - function with one argument
- * @b: pointer to character
+ * binary_to_uint - convert binary string to decimal
+ * @b: binary string
  *
- * Description: converts a binary number to an unsigned integer
- * Return: converted number or 0 if NULL
+ * Return: decimal (unsigned integer)
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int number = 0;
-	unsigned int power = 0;
-	int counter = 0;
+	unsigned int decimal;
+	unsigned int counter;
 
-	if (!b)
-		return (0);
-
-	while (b[counter] != '\0')
-	{
-		if (b[counter] != '\0' && b[counter] != '1')
-			return (0);
-		counter++;
-	}
-	counter -= 1;
-	while (counter >= 0)
+	for (decimal = 0, counter = 0; b[counter] != '\0'; counter++)
 	{
 		if (b[counter] == '1')
-			number += _power_recursion(2, power);
-		power++;
-		counter--;
+			decimal = (decimal << 1) | 1;
+		else if (b[counter] == '0')
+			decimal <<= 1;
+		else if (b[counter] != '0' && b[counter] != '1')
+			return (0);
 	}
-	return (number);
-}
 
-/**
- * _power_recursion - function with two arguments
- * @x: int type first argument
- * @y: int type second argument
- *
- * Description: returns the value of x raised to power of y
- * Return: value of x
- */
-int _power_recursion(int x, int y)
-{
-	if (y == 0)
-		return (1);
-	else if (y < 0)
-		return (-1);
-	else
-		return (x * _power_recursion(x, y - 1));
+	return (decimal);
 }
