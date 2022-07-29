c2c093e0ad1081dbfc3b3c275dbc9991409e2340
Kitesa <kitesamerga@gmail.com>
2022-07-29 05:55:00 -0700

    Updated header file content

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -43,4 +43,5 @@ char *hash_table_get(const hash_table_t *ht, const char *key);
 
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
+void free_hash_list(hash_node_t *head);
 #endif
